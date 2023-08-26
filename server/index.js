8ed8165b2f447148a50436bb5f3f86c99cd13652
adarshKhedekar <108612612+adarshKhedekar@users.noreply.github.com>
2023-08-26 14:16:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,13 @@ const upload = multer();
 
 
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: [""],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
